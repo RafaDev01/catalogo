034ecdaefdfaad506bb2d6bf8fe5b80e15c8b0ad
Rafael do Nascimento Santos <rflwwe123@hotmail.com>
2023-11-03 02:33:34 -0300

    commit

--- js/categorias.js
+++ js/categorias.js
@@ -2,6 +2,10 @@ class Categoria {
 
     static arrayCategorias = [
         { nome: "Animes" , tema: ["Dragon Ball Z", "Naruto", "One Piece", "Attack on Titan"]},
+        { nome: "Memes" , tema: ["Flork"]},
+        { nome: "Marvel" , tema: ["Diversos", "Homem Aranha", "Homem de Ferro"]},
+        { nome: "Jogos" , tema: ["Free Fire"]},
+        { nome: "DC" , tema: ["Arlequina", "Coringa", "Flash", "Jovens Titans", "Lanterna Verde", "Diversos"]},
         { nome: "Nao tem nada aqui é so teste" , tema: ["xamppp", "Naruto", "One Piece", "Attack on Titan"]},
         { nome: "Datas comemorativas", tema: ["Dia das maes", "Dia dos namorados", "Natal", "Páscoa","Dia dos pais", "Halloween"]},
         {nome: "Desenhos", tema: ["Barbie" , "Bojack Horseman", "Futurama", "Lilo e Stitch", "Ateste", "Mario", "Meninas Super Poderosas", "Mickey", "Minions", "Princesas", "Rick and Morty", "Simpsons", "South Park", "Timão e Pumba", "Turma da Mônica"]},
